da46bc433da8f258b2b12397809f5fb5084ffe62
sage-jordan <sagemjordan@gmail.com>
2020-05-21 02:13:38 -0400

    removed

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from './Nav';
 import ReactDOM from 'react-dom'
 import './App.css';
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Nav />
       </header>
     </div>
   );
--- blog-app/src/Nav.js
+++ blog-app/src/Nav.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-function Nav() {
 
+function Nav() {
+    return (
+        <
+    )
 }
 
 export default Nav;
