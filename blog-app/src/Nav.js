9fe52795c761407f0b92761288ad93ae5ac756b2
sage-jordan <sagemjordan@gmail.com>
2020-05-21 02:28:21 -0400

    png?

--- blog-app/src/Nav.js
+++ blog-app/src/Nav.js
@@ -4,7 +4,7 @@ import { Header, Image } from 'semantic-ui-react'
 const Nav = () => {
     return (
         <Header as='h2'>
-            <Image circular src='./assets/headshot2_edited.JPG' />Sage Jordan
+            <Image circular src='./assets/headshot2_edited.png' />Sage Jordan
         </Header>
     )
 }
