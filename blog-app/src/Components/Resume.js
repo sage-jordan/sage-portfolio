8c0fcdf907004406763cd17f464492d150dcf29c
sage-jordan <sagemjrodan@gmail.com>
2020-05-23 15:17:53 -0400

    2nd page in resume, del commented code

--- blog-app/src/Components/Resume.js
+++ blog-app/src/Components/Resume.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Segment, Image, Container } from 'semantic-ui-react'
-import pdf from '../assets/resume1.png'
+import pdf from '../assets/resume1.png';
+import pdf2 from '../assets/resume2.png';
 
 const Resume = () => {
 
@@ -9,6 +10,7 @@ const Resume = () => {
             <Segment color='teal' padded='very'>
                 <Segment color='teal'>
                     <Image src={pdf} fluid />
+                    <Image src={pdf2} fluid />
                 </Segment>
             </Segment>
         </Container >
