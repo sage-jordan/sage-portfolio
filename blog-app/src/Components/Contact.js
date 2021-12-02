5d1e986a5ece51c1aefc6b554d8ea041e42251a4
sage-jordan <sagemjordan@gmail.com>
2021-12-02 14:27:43 -0600

    added contact page, in progress

--- blog-app/src/Components/Contact.js
+++ blog-app/src/Components/Contact.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Container, Segment, Image, Header } from 'semantic-ui-react';
-import programmer from '../assets/teal/undraw_programmer.png'
+import programmer from '../assets/teal/undraw_programmer.png';
+import github from '../assets/icons/GitHub-Mark.png';
 
 const Projects = () => {
     return (
         <Container>
             <Segment>
-                <Header as='h2' color='teal'> Contact  page is coming soon! </Header>
+                <Header as='h2' color='teal'> Contact me </Header>
+                <p><a href="https://www.linkedin.com/in/sage-jordan" target="_blank">
+                    LinkedIn
+                </a></p>
+                <p><a href="https://github.com/sage-jordan" target="_blank">
+                    Github
+                </a></p>
+                <p><a href="mailto:sagemjordan@gmail.com" target="_blank">
+                    Email
+                </a></p>
+                <p><a href="https://medium.com/@sagemjordan" target="_blank">
+                    Medium
+                </a></p>
                 <Image src={programmer} />
             </Segment>
         </Container>
